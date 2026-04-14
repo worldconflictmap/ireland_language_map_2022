962b2fa4504bf5b92048e35ac4da8343bc71dc36
worldconflictmap <140384925+worldconflictmap@users.noreply.github.com>
2026-04-15 04:27:09 +0900

    rollback

--- layers/layers.js
+++ layers/layers.js
@@ -34,15 +34,15 @@ var wms_layers = [];
             })
         });
 
-          var lyr_OSMStandard_3 = new ol.layer.Tile({
+        var lyr_OSMStandard_3 = new ol.layer.Tile({
             'title': 'OSM Standard',
-            'opacity': 1.000000
-
-
+            'opacity': 1.000000,
+            
+            
             source: new ol.source.XYZ({
             attributions: ' &nbsp &middot; <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>',
                 url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
-            }) 
+            })
         });
 var format_English_Other_30_true_4 = new ol.format.GeoJSON();
 var features_English_Other_30_true_4 = format_English_Other_30_true_4.readFeatures(json_English_Other_30_true_4, 
